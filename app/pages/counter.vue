71a7e93e45925c2e0db82f8cbef0f02b3c4e2f5b
betogroo <luizhumberto@gmail.com>
2025-05-23 18:52:51 -0300

    feat: create main v-card

--- app/pages/counter.vue
+++ app/pages/counter.vue
@@ -1,3 +1,14 @@
 <template>
-  <div><h1 class="text=h4 text-center mb-6">Limitador de Acessos</h1></div>
+  <div>
+    <h1 class="text-h4 text-center mb-2">Limitador de Acessos</h1>
+    <v-card
+      class="mx-auto pa-5"
+      color="primary"
+      max-width="400"
+      rounded="lg"
+      variant="tonal"
+    >
+      <v-card-text> Aqui é o conteúdo principal do v-card </v-card-text>
+    </v-card>
+  </div>
 </template>
