d8f1a68a135b03bed495af051c251d4aa0aa6cbf
betogroo <luizhumberto@gmail.com>
2025-05-23 16:03:36 -0300

    refactor: remove nav from  pages

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <nav>
-      <ul>
-        <li><nuxt-link to="/">Home</nuxt-link></li>
-        <li><nuxt-link to="/about">About</nuxt-link></li>
-      </ul>
-    </nav>
     <h1>About</h1>
   </div>
 </template>
