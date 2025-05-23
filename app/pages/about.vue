9b09f6fedf9e1bfa775b87891656ec40c4221014
betogroo <luizhumberto@gmail.com>
2025-05-23 14:41:27 -0300

    feat: add nav bar on index and about pages

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,3 +1,11 @@
 <template>
-  <div><h1>About</h1></div>
+  <div>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+      </ul>
+    </nav>
+    <h1>About</h1>
+  </div>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+      </ul>
+    </nav>
     <h1>Home Page</h1>
   </div>
 </template>
