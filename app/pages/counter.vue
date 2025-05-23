92a740846cef451d38f355a2e16aab1a1f1cfee6
betogroo <luizhumberto@gmail.com>
2025-05-23 18:56:42 -0300

    feat: create child card (diplay)

--- app/pages/counter.vue
+++ app/pages/counter.vue
@@ -3,12 +3,23 @@
     <h1 class="text-h4 text-center mb-2">Limitador de Acessos</h1>
     <v-card
       class="mx-auto pa-5"
-      color="primary"
+      color="success"
       max-width="400"
       rounded="lg"
       variant="tonal"
     >
-      <v-card-text> Aqui é o conteúdo principal do v-card </v-card-text>
+      <v-card-text>
+        <v-card
+          class="mx-auto text-center mt-3 pa-4"
+          color="success"
+          max-width="280"
+          rounded="lg"
+        >
+          <h1 class="text-h2">17</h1>
+          <h2 class="text-h6">Pessoas Entraram</h2>
+          <h4 class="text-caption">max: 50</h4>
+        </v-card>
+      </v-card-text>
     </v-card>
   </div>
 </template>
