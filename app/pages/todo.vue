fb01053f58296ade9c4110c737b559f06417e3c1
betogroo <luizhumberto@gmail.com>
2025-06-02 14:59:52 -0300

    feat: add index

--- app/pages/todo.vue
+++ app/pages/todo.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
   const { list } = useTodo()
-  const click = (item: string) => console.log('clicou', item)
+  const click = (item: string, index: number) =>
+    console.log('clicou', item, index)
 </script>
 <template>
   <h1>Todo List</h1>
   <v-list>
     <v-list-item
-      v-for="item in list"
+      v-for="(item, i) in list"
       :key="item.name"
       prepend-icon="mdi-check"
       :title="item.name"
-      @click="click(item.name)"
+      @click="click(item.name, i)"
     />
   </v-list>
 </template>
