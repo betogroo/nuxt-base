49ba2c68c1eb0a85dba75754693172591319947f
betogroo <luizhumberto@gmail.com>
2025-06-05 15:05:16 -0300

    chore: add vueuse module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', 'vuetify-nuxt-module'],
+  modules: ['@nuxt/eslint', 'vuetify-nuxt-module', '@vueuse/nuxt'],
   future: {
     compatibilityVersion: 4,
   },
