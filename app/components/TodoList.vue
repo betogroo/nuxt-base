20d20d0ad3344d362dabbf5e054bea85240a0126
betogroo <luizhumberto@gmail.com>
2025-06-05 08:33:32 -0300

    feat: past v-list on component and import togle function

--- app/components/TodoList.vue
+++ app/components/TodoList.vue
@@ -1,5 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const { toggleCheck } = useTodo()
+</script>
 
 <template>
-  <div>List</div>
+  <v-list>
+    <v-list-item
+      v-for="item in uncheckedList"
+      :key="item.name"
+      :class="item.checked ? 'text-error ' : 'text-success'"
+      :prepend-icon="
+        item.checked ? 'mdi-checkbox-marked' : 'mdi-checkbox-blank-outline'
+      "
+      :ripple="false"
+      @click="toggleCheck(item.id)"
+    >
+      <template #title>
+        <span :class="item.checked ? 'text-decoration-line-through' : ''">{{
+          item.name
+        }}</span>
+      </template>
+    </v-list-item>
+  </v-list>
+  <v-list />
 </template>
