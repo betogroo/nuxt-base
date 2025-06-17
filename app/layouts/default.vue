2f5fae704dd55b23addafcf1e16000d7c94f8b41
betogroo <luizhumberto@gmail.com>
2025-06-17 14:26:29 -0300

    create menu router array

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+  interface NavItem {
+    title: string
+    route: string
+  }
+  const navItems: NavItem[] = [
+    {
+      title: 'Home',
+      route: '',
+    },
+    {
+      title: 'Limitador de Acessos',
+      route: 'counter',
+    },
+    {
+      title: 'Todo',
+      route: 'todo',
+    },
+    {
+      title: 'About',
+      route: 'about',
+    },
+  ]
+</script>
 <template>
   <v-app>
     <v-app-bar color="primary">
@@ -5,26 +29,25 @@
       <v-app-bar-title>My Nuxt App</v-app-bar-title>
       <v-spacer />
       <v-btn
+        v-for="item in navItems"
+        :key="item.title"
         text
-        to="/"
-        >Home</v-btn
-      >
-      <v-btn
-        text
-        to="/counter"
-        >Limitador de Acessos</v-btn
-      >
-      <v-btn
-        text
-        to="/todo"
-        >Todo</v-btn
-      >
-      <v-btn
-        text
-        to="/about"
-        >About</v-btn
+        :to="`/${item.route}`"
+        >{{ item.title }}</v-btn
       >
     </v-app-bar>
+    <v-navigation-drawer>
+      <template #prepend>
+        <v-list-item
+          lines="three"
+          prepend-avatar="https://avatar.iran.liara.run/public/boy"
+          subtitle="sergioaroudo@gmail.com"
+          title="Sérgio Moreno"
+        />
+      </template>
+
+      <v-divider />
+    </v-navigation-drawer>
     <v-main>
       <v-container>
         <slot />
