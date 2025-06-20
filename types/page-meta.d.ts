73222d56fc841921fbc0c4a2f93aa74fbac3702e
betogroo <luizhumberto@gmail.com>
2025-06-20 13:37:12 -0300

    chore: rename global to page-meta

--- types/page-meta.d.ts
+++ types/page-meta.d.ts
@@ -3,11 +3,9 @@ import type { PageMeta as NuxtPageMeta } from '#app'
 
 declare module '#app' {
   interface PageMeta extends NuxtPageMeta {
-    // Adicione suas propriedades personalizadas aqui
     title: string
     icon?: string
     order?: number
     hideInNav?: boolean
-    // outras propriedades...
   }
 }
