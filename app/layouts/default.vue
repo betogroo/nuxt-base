851875a1f3e0ef186c5369398e38a02b5d185e60
betogroo <luizhumberto@gmail.com>
2025-06-23 10:30:33 -0300

    refactor(NavDrawer): move logic to composable

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,21 +1,5 @@
 <script setup lang="ts">
-  const router = useRouter()
-  const navItems = computed(() =>
-    router
-      .getRoutes()
-      .filter((route) => route.meta.title)
-      .sort((a, b) => (a.meta.order || 0) - (b.meta.order || 0))
-      .map((route) => ({
-        title: route.meta.title,
-        icon: route.meta.icon,
-        path: route.path,
-      })),
-  )
-
-  const isDrawerActive = ref(false)
-  const toggleDrawer = () => {
-    isDrawerActive.value = !isDrawerActive.value
-  }
+  const { navItems, toggleDrawer, isDrawerActive } = useNavDrawer()
 </script>
 <template>
   <v-app>
